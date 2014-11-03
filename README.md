aebef64c7bbf929cb337bb361b0ec9fbfa8f9605
Eric Saupe <ericsaupe@gmail.com>
2014-11-02 20:50:14 -0700

    Updated for better scraping

--- README.md
+++ README.md
@@ -4,6 +4,43 @@ Scrapes Twitter for data attached to a certain hashtag or user mention.
 Mentions in the tweet are saved to a database to be evaluated later.
 
 ## Installation
-1. Download project to your machine
-2. Install all requirements in `requirements.txt`
-5. Run `python twitter-scraper.py`
+### MongoDB and Databases
+1. Install MongoDB, [http://docs.mongodb.org/manual/installation/](http://docs.mongodb.org/manual/installation/)
+2. Create `dev_tweets` and `tweets` databases and users
+  * `mongo`
+  * `use dev_tweets`
+  * ```
+    db.createUser(
+                  {
+                    user: 'twitterScraper', 
+                    pwd: 'twitterScraper', 
+                    roles: [ "readWrite", "dbAdmin" ]
+                  }
+                )
+    ```
+  * `use tweets`
+  * ```
+    db.createUser(
+                  {
+                    user: 'twitterScraper', 
+                    pwd: 'twitterScraper', 
+                    roles: [ "readWrite", "dbAdmin" ]
+                  }
+                )
+    ```
+### Project Setup
+1. Clone project to your machine
+2. Create a virtualenv for twitter-scraper
+3. Start that virtualenv
+2. `pip install -r requirements.txt`
+3. Edit `twitter-scraper.py`
+  1. Enter your Twitter Application Keys, [https://apps.twitter.com](https://apps.twitter.com)
+  2. Enter what you would like to be watching for on Twitter
+  3. Verify MongoDB users and database address
+  4. Save
+4. Run `python twitter-scraper.py`
+  * For developer mode, to save to dev_tweets, run with `-d true`
+  * For verbose output to console run with `-v true`
+
+## Developing
+Clone the repository and open a pull request to be merged back in.
--- twitter-scraper.py
+++ twitter-scraper.py
@@ -4,6 +4,47 @@ from pymongo import MongoClient
 import datetime
 import threading
 import logging
+import sys
+import getopt
+
+# ==================================================
+# STEP ONE - APP KEYS
+# ==================================================
+#
+# Set up App Keys
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN_KEY = ''
+ACCESS_TOKEN_SECRET = ''
+
+# ==================================================
+# STEP TWO - WHAT TO TRACK, HASHTAGS, MENTIONS, ETC.
+# ==================================================
+#
+# Set what to watch for
+TRACK_BY = ['#twitterscraper', '@twitter']
+# Auto reply to users who use these tags?
+AUTO_REPLY = False
+
+# ==================================================
+# STEP THREE - VERIFY MONGODB LOGIN
+# ==================================================
+#
+# Set passwords for your databases
+DEV_DB_USER = 'twitterScraper'
+DEV_DB_PASSWORD = 'twitterScraper'
+DB_USER = 'twitterScraper'
+DB_PASSWORD = 'twitterScraper'
+# Set up Mongodb
+client = MongoClient('mongodb://localhost')
+# Set Collections, these get set in main
+TWEETS = None
+USERS = None
+
+# Are we Developing?
+DEVELOPER_MODE = False
+# Verbose Output?
+VERBOSE = False
 
 
 class TwitterStreamer(TwythonStreamer):
@@ -18,40 +59,48 @@ class TwitterStreamer(TwythonStreamer):
             has_coords = False
             if tweet['coordinates'] or tweet['place']:
                 has_coords = True
-                logging.info('Tweet has coordinates!')
+                coords_message = 'Tweet has coordinates!'
+                logging.info(coords_message)
+                if VERBOSE:
+                    print coords_message
             # Save Tweet
-            tweets.insert(tweet)
+            TWEETS.insert(tweet)
             # Save Mentions
-            save_mentions(tweet)
+            self.save_mentions(tweet)
 
     def on_error(self, status_code, data):
+        logging.error(status_code)
         print status_code
 
-    def save_mentions(tweet):
+    def save_mentions(self, tweet):
         for mention in tweet['entities']['user_mentions']:
-            screen_name = users.find_one({'twitter': mention['screen_name']})
-            # If there is a truck, and it is a truck, and the user forgot the coordinates, let's let them know
-            if SEND_MESSAGE:
+            screen_name = USERS.find_one({'twitter': mention['screen_name']})
+            if AUTO_REPLY and not DEVELOPER_MODE:
                 # Send message to user if not in DEVELOPER_MODE
-                logging.info('Sending message to @%s. %s' % (tweet['user']['screen_name'], datetime.datetime.now()))
-                if not DEVELOPER_MODE:
-                    message_thread = TweetMaker(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET,
-                                                tweet['user']['screen_name'])
-                    message_thread.start()
+                send_message = 'Sending message to @%s. %s' % (tweet['user']['screen_name'], datetime.datetime.now())
+                logging.info(send_message)
+                if VERBOSE:
+                    print send_message
+                message_thread = TweetMaker(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET,
+                                            tweet['user']['screen_name'])
+                message_thread.start()
             # If no user (mention) was found in our database let's save it
-            if not truck:
-                users.insert({
+            if not screen_name:
+                USERS.insert({
                     'twitter': mention['screen_name'],
                     'created': datetime.datetime.utcnow(),
-                    'added_by': tweet['user']['screen_name']
+                    'mentioned_by': tweet['user']['screen_name']
                 })
-                logging.info('Added @%s! %s' % (mention['screen_name'], datetime.datetime.now()))
+                added_message = 'Added @%s! %s' % (mention['screen_name'], datetime.datetime.now())
+                logging.info(added_message)
+                if VERBOSE:
+                    print added_message
 
 
 class TweetMaker(threading.Thread):
     """
     TweetMaker is a self-contained thread for sending a message via Twitter.  It uses whatever access token
-    you give it but by default in here we will be using the Twitter account for sending tweets.
+    you give it.
     """
     def __init__(self, consumer_key, consumer_secret, access_token_key, access_token_secret, mention):
         threading.Thread.__init__(self)
@@ -64,48 +113,74 @@ class TweetMaker(threading.Thread):
 
     def run(self):
         try:
+            # Update to whatever message you want autosent back
             self.twitter.update_status(status='@%s, thanks for the mention!' % self.mention)
         except:
-            logging.error('Failed to send tweet to @%s! %s' % (self.mention, datetime.datetime.now()))
+            error_message = 'Failed to send tweet to @%s! %s' % (self.mention, datetime.datetime.now())
+            logging.error(error_message)
+            if VERBOSE:
+                print error_message
 
-# Are we Developing?
-DEVELOPER_MODE = True
+def run_stream():
+    stream = TwitterStreamer(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+    started_message = 'Started Twitter Scraper!!! %s' % datetime.datetime.now()
+    logging.info(started_message)
+    if VERBOSE:
+        print started_message
 
-# Set up App Keys
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_TOKEN_KEY = ''
-ACCESS_TOKEN_SECRET = ''
+    if DEVELOPER_MODE:
+        # Get Worldwide trending topics to filter by for development
+        twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+        trending = twitter.get_place_trends(id=1)[0]['trends']
+        global TRACK_BY
+        TRACK_BY = []
+        for trend in trending:
+            TRACK_BY.append(trend['name'])
+    if VERBOSE:
+        print 'Scraping tweets with any of the following: %s' % TRACK_BY
+    stream.statuses.filter(track=TRACK_BY)
+    
 
-# Start Logging
-logging.basicConfig(filename='twitter-scraper.log', level=logging.DEBUG)
-logging.info('Started Twitter Scraper!!! %s' % datetime.datetime.now())
+def main(argv):
+    try:
+      opts, args = getopt.getopt(argv,"hd:v:")
+    except getopt.GetoptError:
+        print 'twitter-scraper.py -d <development_mode> -v <verbose>'
+        sys.exit(2)
+    for opt, arg in opts:
+      if opt == '-h':
+        print 'twitter-scraper.py -d <development_mode> -v <verbose>'
+        sys.exit()
+      elif opt in ("-d"):
+        if arg == 'true':
+            global DEVELOPER_MODE
+            DEVELOPER_MODE = True
+            print 'Developer Mode ENGAGED!'
+      elif opt in ("-v"):
+        if arg == 'true':
+            global VERBOSE
+            VERBOSE = True
+            print 'PRINT ALL THE THINGS!!!'
 
-# Set up Mongodb
-client = MongoClient('mongodb://localhost:12945')
+    # Start Logging
+    logging.basicConfig(filename='twitter-scraper.log', level=logging.DEBUG)
 
-# Set DBs
-if DEVELOPER_MODE:
-    authenticated = client.dev_tweets.authenticate('scriptUser', 'TwitterTr4ck3r')
-    db = client.dev_tweets
-else:
-    authenticated = client.tweets.authenticate('scriptUser', 'TwitterTr4ck3r')
-    db = client.tweets
+    # Set DBs
+    if DEVELOPER_MODE:
+        authenticated = client.dev_tweets.authenticate(DEV_DB_USER, DEV_DB_PASSWORD)
+        db = client.dev_tweets
+    else:
+        authenticated = client.tweets.authenticate(DB_USER, DB_PASSWORD)
+        db = client.tweets
 
-# Set Collections
-tweets = db.tweets
-users = db.users
+    # Set Collections
+    global TWEETS
+    TWEETS = db.tweets
+    global USERS
+    USERS = db.users
 
-# BEGIN THE STREAM!!!
-stream = TwitterStreamer(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-# FILTER BY ALL THE THINGS!!!
-track_by = ['#twitterscraper']
-if DEVELOPER_MODE:
-    # Get Worldwide trending topics to filter by for development
-    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-    trending = twitter.get_place_trends(id=1)[0]['trends']
-    track_by = []
-    for trend in trending:
-        track_by.append(trend['name'])
-else:
-    stream.statuses.filter(track=track_by)
+    # BEGIN THE STREAM!!!
+    run_stream()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
